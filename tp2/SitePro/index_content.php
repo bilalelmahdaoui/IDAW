8fbd06ad9b09a77794eb81613977410d37d23bf0
bilalelmahdaoui <bilalelmahdaoui@gmail.com>
2021-03-13 20:39:17 +0100

    factorisation (ex03)

---
+++ tp2/SitePro/index_content.php
@@ -0,0 +1,91 @@
+<section class="home_section">
+      <h2>Introduction</h2>
+      <img
+        class="home_image"
+        src="https://scontent-cdt1-1.xx.fbcdn.net/v/t1.0-9/129524849_3638471702863205_1266893178302400808_o.jpg?_nc_cat=106&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=pMgqWQFBIj0AX_C78d1&_nc_ht=scontent-cdt1-1.xx&oh=95076cd9867e7dbce8dcb3732677ba22&oe=606D3F4A"
+      />
+      <p class="content">
+        Je m’appelle Bilal El Mahdaoui, j’ai 21 ans et je suis actuellement
+        élève ingénieur généraliste à L’institut Mines Telecom Lille Douai dans
+        le domaine "Sciences et technologies du numérique"
+      </p>
+      <p>
+        Je suis étudiant dans le cadre du partenariat de diplôme diplôme entre
+        l’IMT Lille Douai et mon école d’ingénieur précédente au Maroc :
+        l’Académie Internationale Mohammed VI (AIAC), dans laquelle j’ai passé 2
+        années dans le Cycle Ingénieur - Génie Informatique. Je suis à la
+        recherche d'une entreprise qui pourrait m'accompagner dans le cadre d'un
+        stage technique d'une durée de 4 à 6 mois et pouvant débuter à partir du
+        1er Février 2021.
+      </p>
+      <p>
+        En bref je dirais que ce qui me caractérise le plus est la créativité et
+        l'autonomie dans l’apprentissage et la résolution des problèmes. Je suis
+        aussi quelqu’un qui apprécie le fait de rencontrer des problèmes que de
+        toujours avoir des solutions prêtes, parce ca me permet de bien
+        maîtriser la chose et aussi de découvrir d’autres domaines.
+      </p>
+      <p>
+        En ce qui concerne le coté para-universitaire, durant mon parcours
+        universitaire, j’ai pu intégrer différents clubs : Enactus, le club
+        sociale, le club Forum AIAC- Entreprises et finalement le club
+        informatique dont j’ai pu décrocher le poste président et dans lequel
+        j’ai passé certains des meilleurs moments de mon parcours, j’ai appris
+        beaucoup de choses, comment gérer une équipe, gérer des réunions,
+        comment organiser de grands évènements, les stigmatiser, et les
+        médiatiser proprement. - Durant mon temps libre. Je fais un peu du
+        sport, du dessin digital, de la photographie, je joue un peu de la
+        musique, et j’essaye toujours de découvrir de nouvelles compétences et
+        les apprendre. En fait durant le dernier confinement, j’ai pu apprendre
+        tout un nouveau langage de programmation ainsi qu’un Framework de
+        développement et avec lequel j’ai travailler sur des projets personnels
+      </p>
+      <p>
+        Mais au cours des 5 dernières années, je me suis concentré à apprendre
+        des notions du Design : tout ce qui est conception des visuels
+        graphiques et montage vidéo. Au début c’était juste un loisir que je
+        pratiquais de temps en temps, mais dans ces 2 dernières années, j’ai
+        exploiter mes compétences pour faire du freelance parallèlement avec mes
+        études, j’ai travaillé avec pas mal d’entreprises et associations sur
+        plusieurs projets et ils ont apprécié la manière avec laquelle je
+        travaille.
+      </p>
+    </section>
+
+    <br />
+
+    <section class="home_section">
+      <a href="bilaldesignfirm.com" target="_blank">
+        <h2>bilaldesignfirm.com</h2>
+      </a>
+      <img
+        class="bdf_image_mobile"
+        src="https://media.giphy.com/media/qaK6lP6VIv3EJe1KKV/giphy.gif"
+      />
+      <img
+        class="bdf_image_desktop"
+        src="https://media.giphy.com/media/Wpk2Nug2MbDru9diyO/source.gif"
+      />
+      <p>
+        Au cours des 5 dernières années, Je m'étais formé sur Internet et
+        j'avais appris des notions du Graphic Design : tout ce qui est
+        conception des visuels graphiques et montage vidéo. Au début cela était
+        juste un loisir que je pratiquais de temps en temps, mais dans ces 2
+        dernières années, j’ai commencé à exploiter mes compétences dans ce
+        domaine là pour faire du freelance parallèlement avec mes études, j’ai
+        travaillé avec pas mal d’entreprises et associations au Maroc sur
+        plusieurs projets et ils ont apprécié la manière avec laquelle je
+        travaille. Donc cela m’a permis d’être plus autonome financièrement.
+      </p>
+      <p>
+        Dernièrement j’avais cette idée de construire ma propre marque que j’ai
+        nommé “bilaldesignfirm”, qui est principalement axé ce que je faisais
+        avant : la construction de l'identité graphique, et les visuels de
+        médiatisation sur les réseaux sociaux. En plus de ça, je viens aussi de
+        lancer une boutique en ligne des marchandise imprimée (T-shirts,
+        capuchons, les sacs, les pochettes des telephones sur dans les
+        plateformes Merch by Amazon, Printify et Redbubble. Donc ce que je fais
+        moi c’est que je construit le design, et la plateforme prend charge de
+        l’impression sur des t shirts
+      </p>
+    </section>

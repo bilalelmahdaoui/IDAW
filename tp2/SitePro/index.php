77a55ace27b9f5d77e86cc086eb4a4415ae3bb7b
bilalelmahdaoui <bilalelmahdaoui@gmail.com>
2021-03-20 14:37:52 +0100

    Connection done with GET method

--- tp2/SitePro/index.php
+++ tp2/SitePro/index.php
@@ -1,31 +1,31 @@
 <?php
-  require_once("template_header.php");
-  require_once("template_menu.php");
+require_once("template_header.php");
+require_once("template_menu.php");
 
-  $currentPageId = 'accueil';
-  $lang = 'fr';
-  if(isset($_GET['page'])) {
-    $currentPageId = $_GET['page'];
-  }
-  if(isset($_GET['lang']) && ($_GET['lang'] == 'en' || $_GET['lang'] == 'fr')) {
-    $lang = $_GET['lang'];
-  }
+$currentPageId = 'accueil';
+$lang = 'fr';
+if (isset($_GET['page'])) {
+  $currentPageId = $_GET['page'];
+}
+if (isset($_GET['lang']) && ($_GET['lang'] == 'en' || $_GET['lang'] == 'fr')) {
+  $lang = $_GET['lang'];
+}
 ?>
 
 <?php
-  renderMenuToHTML($currentPageId, $lang);
+renderMenuToHTML($currentPageId, $lang);
 ?>
 
 <section class="corps">
   <?php
-    $pageToInclude = ${lang} . '/' . $currentPageId . '.php';
-    if(is_readable($pageToInclude))
-      require_once($pageToInclude);
-    else
-      require_once("error.php");
+  $pageToInclude = $lang . '/' . $currentPageId . '.php';
+  if (is_readable($pageToInclude))
+    require_once($pageToInclude);
+  else
+    require_once("error.php");
   ?>
 </section>
 
 <?php
-  require_once("template_footer.php");
+require_once("template_footer.php");
 ?>

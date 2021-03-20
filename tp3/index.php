c641cb09c265c4083cc034dbed6920a82b029e4e
bilalelmahdaoui <bilalelmahdaoui@gmail.com>
2021-03-20 15:46:25 +0100

    gestion des cookies complète

---
+++ tp3/index.php
@@ -0,0 +1,36 @@
+<?php
+$style = "style1";
+if (isset($_COOKIE['css'])) {
+  $style = $_COOKIE['css'];
+}
+if (isset($_GET['css'])) {
+  $style = $_GET['css'];
+  setcookie("css", $style, time() + 86400);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <?php echo "<link rel=\"stylesheet\" href=\"" . $style . ".css\">" ?>
+  <title>Document</title>
+</head>
+
+<body>
+  <h1>Select your theme</h1>
+  <p>You are currently using the <strong><?php echo $style; ?> theme.</strong></p>
+  <form id="style_form" action="index.php" method="GET">
+    <select name="css">
+      <option value="dark">dark</option>
+      <option value="light">light</option>
+    </select>
+    <input type="submit" value="Change theme" />
+  </form>
+</body>
+
+</html>
